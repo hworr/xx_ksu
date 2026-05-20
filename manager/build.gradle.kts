ff76c3106f82b3813303acb6ca4a686bbb8d1d1b
skye-tachyon <sonicgames772@gmail.com>
2026-05-20 14:31:23 -0400

    manager: bump versioning
    * ik
    
    Signed-off-by: skye-tachyon <sonicgames772@gmail.com>
    Signed-off-by: //. <174224977+skye-tachyon@users.noreply.github.com>

--- manager/build.gradle.kts
+++ manager/build.gradle.kts
@@ -27,7 +27,7 @@ fun getGitDescribe(): String {
 
 fun getVersionCode(): Int {
     val commitCount = getGitCommitCount()
-    return 30000 + commitCount - 91
+    return 31000 + commitCount
 }
 
 fun getVersionName(): String {
